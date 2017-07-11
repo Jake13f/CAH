15c698f56104d2d7fdef1fe3ea93c6934881a563
Jacob Feliciani <jake13f@hotmail.com>
2017-07-11 16:42:22 -0400

    removed unneccessary duplicate file

--- public/js/game/socketio/socket.js
+++
@@ -1,9 +0,0 @@
-const PROTOCOL = window.location.protocol;
-const HOSTNAME = window.location.hostname;
-const BASE_URL = PROTOCOL + "//" + HOSTNAME;
-
-$(document).ready(() => {
-  console.log(BASE_URL);
-  const socket = io(BASE_URL);
-  console.log(socket);
-});
